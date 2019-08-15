5b0c171dc3f0d44760adee6177cf12adacc4a9aa
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-08-15 12:56:22 -0600

    feat: initial commit

---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+
+const JSONLDSerializer: jest.SnapshotSerializerPlugin = {
+  test(val) {
+    return (
+      val &&
+      val.$$typeof === Symbol.for("react.element") &&
+      val.type === "script" &&
+      val.props.dangerouslySetInnerHTML &&
+      val.props.type &&
+      val.props.type === "application/ld+json"
+    );
+  },
+
+  print(val, serialize) {
+    const { dangerouslySetInnerHTML, ...tag } = val.props;
+
+    return serialize(
+      <script {...tag}>
+        {serialize(JSON.parse(dangerouslySetInnerHTML.__html))}
+      </script>
+    );
+  }
+};
+
+module.exports = JSONLDSerializer;
